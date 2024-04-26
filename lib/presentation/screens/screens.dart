05a98c1baa8c134e5082253851a95bfdbb3de009
Fernando Herrera <fernando.herrera85@gmail.com>
2024-04-26 16:02:54 -0400

    Refactor todo_screen.dart to use SegmentedButton for filtering guests

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,5 +1,5 @@
 export '01_state_provider/state_provider_screen.dart';
-export '04_todo/todo_screen.dart';
+export '03_todo/todo_screen.dart';
 export '05_future_provider/future_provider_screen.dart';
 export '06_family_future_provider/future_provider_screen.dart';
 export '07_stream_provider/stream_provider_screen.dart';
